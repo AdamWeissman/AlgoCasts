14131b4d57c214611e73b7fdb846868670738bea
AdamWeissman <adam@comedytragedyepic.com>
2020-08-15 01:16:52 -0400

    maxChar and palindromes multiple solutions

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charactersMap = {};
+  let max = 0;
+  //let maxChar = '';
+  for (let character of str) {
+    charactersMap[character] = charactersMap[character] + 1 || 1
+  };
+  
+  for (let character in charactersMap) {
+    if (charactersMap[character] > max) {
+      max = charactersMap[character];
+      maxChar = character;
+    }
+  }
+  return maxChar 
+}
 
 module.exports = maxChar;
