2a0b55778eab653c1b50ed1f3181c11a4a5378e1
AdamWeissman <adam@comedytragedyepic.com>
2020-08-15 16:22:50 -0400

    worked through anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(facialRecognitionCaptureA, facialRecognitionCaptureB) {
+
+  const faceParser = (facialFeatures) => { // this is the same as The Snitch
+    facialFeaturesBrokenDown = {};
+    for (let feature of facialFeatures.replace(/[^\w]/g, "").toLowerCase()) {
+      facialFeaturesBrokenDown[feature] = facialFeaturesBrokenDown[feature] + 1 || 1
+    }
+  return facialFeaturesBrokenDown
+  }
+
+  const faceDataScrubbedA = faceParser(facialRecognitionCaptureA)
+  const faceDataScrubbedB = faceParser(facialRecognitionCaptureB)
+
+  console.log(faceDataScrubbedA)
+  console.log(faceDataScrubbedB)
+
+  if (Object.keys(faceDataScrubbedA).length !== Object.keys(faceDataScrubbedB).length) { 
+    return false;
+  } 
+
+  for (let val in faceDataScrubbedA) {
+    if (faceDataScrubbedA[val] !== faceDataScrubbedB[val]) {
+      console.log(faceDataScrubbedB[val])
+      return false
+    }
+  }
+   
+  return true;
+  
+}
+
+// function anagrams(str1, str2) {
+//   const cleaner = (str) => {
+//     return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+//   }
+
+//   return cleaner(str1) === cleaner(str2)
+// }
+
+
 
 module.exports = anagrams;
