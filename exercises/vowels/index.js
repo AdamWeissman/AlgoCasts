2603f36b4e4c1ce52a8f1eadf58522812032a0ff
AdamWeissman <adam@comedytragedyepic.com>
2020-08-15 17:31:28 -0400

    vowels with regex

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,16 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// function vowels(str) {
+//   vowels = ["a","e","i","o","u"]
+//   the_string = str.toLowerCase()
+//   counter = 0
+//   for (let i of the_string.split("")) {
+//     if (vowels.includes(i)) {
+//       counter+=1;
+//     }
+//   }
+//   return counter
+// }
+
 function vowels(str) {
-  vowels = ["a","e","i","o","u"]
-  the_string = str.toLowerCase()
-  counter = 0
-  for (let i of the_string.split("")) {
-    if (vowels.includes(i)) {
-      counter+=1;
-    }
-  }
-  return counter
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
