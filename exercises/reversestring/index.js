5c651c118640c71abad5ff7e68f0c77236d10afc
AdamWeissman <adam@comedytragedyepic.com>
2020-12-02 00:36:15 -0500

    reverse string with swapping function

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -16,21 +16,41 @@
 //   return str.split('').reverse().join('')
 // }
 
-// function reverse(str) {
-//   let reversed = '';
+function reverse(str) {
+  let reversed = '';
   
-//   for (let character of str) {
-//     reversed = character + reversed;
-//   }
+  for (let character of str) {
+    reversed = character + reversed;
+  }
 
-//   return reversed;
+  return reversed;
+}
+
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed;
+//   }, '')
 // }
 
-function reverse(str) {
-  return str.split('').reduce((reversed, character) => {
-    return character + reversed;
-  }, '')
+// NOTE THE ABOVE WERE FROM OFFICIAL SOLUTIONS AND TESTS PRIOR TO 12/1
+// NOTE BELOW IS FOR PRACTICE AS OF 12.1.2020
+
+function reverse(theString) {
+  let str = theString.split(''),
+    left = 0,
+    right = str.length - 1 
+  
+  while (left < right) {
+    let temp = str[left]
+    str[left] = str[right]
+    str[right] = temp
+    left++;
+    right--;
+  }
+  return str.join('')
 }
+
+
 
 
 
