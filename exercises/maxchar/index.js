91b809f12726190c44e6b415a096f61a29976439
AdamWeissman <adam@comedytragedyepic.com>
2020-12-03 23:48:09 -0500

    too exhausted to figure out how to use object.entries with sort by value

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,21 +5,35 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {
-  const charactersMap = {};
-  let max = 0;
-  //let maxChar = '';
-  for (let character of str) {
-    charactersMap[character] = charactersMap[character] + 1 || 1
-  };
+// OLD SOLUTION BELOW
+// function maxChar(str) {
+//   const charactersMap = {};
+//   let max = 0;
+//   //let maxChar = '';
+//   for (let character of str) {
+//     charactersMap[character] = charactersMap[character] + 1 || 1
+//   };
   
-  for (let character in charactersMap) {
-    if (charactersMap[character] > max) {
-      max = charactersMap[character];
-      maxChar = character;
-    }
+//   for (let character in charactersMap) {
+//     if (charactersMap[character] > max) {
+//       max = charactersMap[character];
+//       maxChar = character;
+//     }
+//   }
+//   return maxChar 
+// }
+
+
+// NEW SOLUTION
+
+function maxChar(str) {
+  let frequencyHash = {}
+  let arr = str.split('')
+  for (val of arr) {
+    frequencyHash[val] += 1
   }
-  return maxChar 
 }
+
+
 
 module.exports = maxChar;
