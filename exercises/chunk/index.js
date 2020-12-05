f55afed2040d91e4cb584498d765ae5b759a0aca
AdamWeissman <adam@comedytragedyepic.com>
2020-12-04 21:24:16 -0500

    slider style solution for chunking array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -37,5 +37,28 @@
 
 // OLD ARRAY CHUNKING METHODS ABOVE ... NEW BELOW
 
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, n) {
+  let chunkedArrays = []
+  let tempArray = []
+  for (let rightSliderClamp = 0; rightSliderClamp <= array.length; rightSliderClamp++){ // <= array.length to capture last element
+    if (tempArray.length == n || rightSliderClamp == array.length ) {
+      chunkedArrays.push(tempArray.slice()) // NEED to use slice, otherwise it returns latest value of tempArray for everything
+      tempArray.length = 0
+    }
+    tempArray.push(array[rightSliderClamp])
+  }
+  return chunkedArrays
+}
+
 
 module.exports = chunk;
