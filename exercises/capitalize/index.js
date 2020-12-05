b7be01d27659abbdf9fa30c33031ac48754245dd
AdamWeissman <adam@comedytragedyepic.com>
2020-12-05 14:38:49 -0500

    edited to capitalize first word of eevery sentence, but sorta not optimal solutoin ... almost hardcoded

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,12 +7,22 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-  const capitalized = []
-  for (let word of str.split(' ')) {
-    capitalized.push(word[0].toUpperCase() + word.slice(1))
+function capitalize(str, wordOrSentence) {
+  if (wordOrSentence === 'everyWord') {
+    const capitalized = []
+    for (let word of str.split(' ')) {
+      capitalized.push(word[0].toUpperCase() + word.slice(1))
+    }
+    return capitalized.join(' ')
+  } else if (wordOrSentence === 'onlySentence') {
+      const capitalizedSentences = []
+      for (let sentence of str.split(/\.\s/) ) {
+        console.log(sentence)
+        capitalizedSentences.push(sentence[0].toUpperCase() + sentence.slice(1))
+      }
+      return capitalizedSentences.join('. ')
   }
-  return capitalized.join(' ')
+   
 }
 
 module.exports = capitalize;
